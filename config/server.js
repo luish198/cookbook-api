c880f04a701c09704922a472373483fe45421bb5
rocnogu <rocnogu@abv.bg>
2021-08-01 11:41:33 +0300

    Delete all the stuff

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '1611e44a004026b489bf6e2e768657c7'),
-    },
-  },
-});
